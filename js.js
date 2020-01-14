332a5290f64f56befe0a1e1e0bd9afaf0b514a72
eliran menachem <eliran.menachem@gmail.com>
2020-01-14 14:29:30 +0200

    Solution

--- js.js
+++ js.js
@@ -1,10 +1,22 @@
 const array = [88, 44, 32, 30, 31, 19, 74];
 
 function average(array, lowValue, highValue) {
+    let sum = 0;
+    let n = 0;
 
+    for (let i = 0; i < array.length; i++) {
+        let num = array[i];
+        if (num >= lowValue && num <= highValue) {
+            console.log('this number is include:  '+num);
+            sum += num;
+            n++
+        }// End if
+    }// End for loop
+    console.log('this is sum: '+ sum);
+    console.log('this is n: '+ n);
+    
+    averageOfNums = sum / n;
+    return averageOfNums
+}// End function
 
-}
-
-
-console.log(average(array, 30, 35)) // 31
-console.log(average(array, 10, 30)) // 24.5
+console.log('Average: '+average(array, 30, 35)) // 31
