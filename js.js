3606154ecf07fc31617269c3213f7b3ab3180392
eliran menachem <eliran.menachem@gmail.com>
2020-01-14 14:15:22 +0200

    Ranged-Average Question

---
+++ js.js
@@ -0,0 +1,10 @@
+const array = [88, 44, 32, 30, 31, 19, 74];
+
+function average(array, lowValue, highValue) {
+
+
+}
+
+
+console.log(average(array, 30, 35)) // 31
+console.log(average(array, 10, 30)) // 24.5
